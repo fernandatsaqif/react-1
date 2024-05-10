0e9de7c5580df8fd31dfcfe3573cf3ecb1463603
fernandatsaqif <f.tsaqif2004@gmail.com>
2024-05-10 21:18:41 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import ReviewItems from './Reviews';
+import PropTypes from 'prop-types';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Parentbox">
+
+      <FotoProduk />
+      <ProdukInfo IsDiskon="yes" name="Nike Air 2" category="TREND MASA KINI" />
+      <ReviewItems />
+
     </div>
   );
 }
+
+
+
+function FotoProduk() {
+  return (
+
+    <div className="Foto">
+      <img src="sneakers2.jpg" />
+    </div>
+
+  );
+}
+
+function CekDiskon(props) {
+  const {IsDiskon, diskon} = props;
+  if (IsDiskon == "yes") {
+    return (
+      <p>Diskon {diskon}% Off</p>
+    );
+  } 
+  else if (IsDiskon == "coming") {
+    return (
+      <p>besok akan ada diskon</p>
+    );
+  }
+  else {
+    return (
+      <p>Belum ada diskon</p>
+    );
+  }
+}
+
+function ProdukInfo(props) {
+  const {category,name,IsDiskon} = props; 
+  const benefits = ["Tidak Kusut Terkena Air", "Bahan Nyaman", "Tidak gampang Bau"];
+  const listBenefits = benefits.map((itemBenefit) =>
+    <li key={itemBenefit}>{itemBenefit}</li>
+)
+  return (
+    <div>
+
+      <div className="Deskripsi">
+        <p className="Cate">{category}</p>
+        <h1 className="Title">{name}</h1>
+        <p className="Price">IDR 12.000.000</p>
+        <CekDiskon IsDiskon={IsDiskon} diskon={50} />
+        <p className="Info">
+          Sepatu Nike Air adalah sepatu olahraga yang ikonik dan inovatif, terkenal karena desainnya yang stylish dan teknologi canggihnya. Desainnya yang futuristik dan ergonomis membuatnya cocok untuk berbagai aktivitas, mulai dari berlari hingga bermain basket.
+        </p>
+        <ul>
+          {listBenefits}
+        </ul>
+        <a onClick={(e) => TambahCart(name, e)} href="#">Add to cart</a>
+      </div>
+
+    </div>
+  );
+}  
+
+function TambahCart(e) {
+  console.log("Membeli " + e);
+}
+
+CekDiskon.propTypes = {
+  diskon: PropTypes.number.isRequired
+};
 
 export default App;
---
+++ src/Reviews.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Reviews() {
+    // mempersiapkan data dummy
+    const users = [
+      {
+        "id": 1,
+        "name": "Agus",
+        "review": "Harganya lumayan merakyat lah hehe",
+        "photo": "https://images.pexels.com/photos/19443255/pexels-photo-19443255/free-photo-of-hitam-dan-putih-pria-gelap-wajah.jpeg?auto=compress&cs=tinysrgb&w=600"
+      },
+      {
+        "id": 2,
+        "name": "Shani",
+        "review": "Harganya lumayan merakyat lah hehe",
+        "photo": "https://images.pexels.com/photos/22885538/pexels-photo-22885538/free-photo-of-wanita-perempuan-kaum-wanita-muda.jpeg?auto=compress&cs=tinysrgb&w=600"
+      },
+      {
+        "id": 3,
+        "name": "Aldo",
+        "review": "Harganya lumayan merakyat lah hehe",
+        "photo": "https://images.pexels.com/photos/20019760/pexels-photo-20019760/free-photo-of-jaket-wajah-muka-potret.jpeg?auto=compress&cs=tinysrgb&w=600"
+      }
+    ];
+    const listReview = users.map((itemReview) =>
+      <div className="item">
+        <img src={itemReview.photo} />
+        <div className="User">
+          <h3>{itemReview.name}</h3>
+          <p>{itemReview.review}</p>
+        </div>  
+      </div>
+    );
+  
+    return (
+      <div className="Review-box">
+      <h2>Reviews</h2>
+        {listReview}
+     </div>
+  
+    );
+  }
+
+export default Reviews ;
